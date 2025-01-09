81d61939deb4c675bc77ad20faccc42d20ab5887
Fabio Acosta <Fabio@Acosta.com>
2025-01-09 20:16:39 +0000

    Relaciones creadas

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,4 +9,14 @@ class Country extends Model
 {
     /** @use HasFactory<\Database\Factories\CountryFactory> */
     use HasFactory;
+
+    public function users()
+    {
+        $this->hasMany(User::class);
+    }
+
+    public function posts()
+    {
+        $this->hasManyThrough(Post::class, User::class);
+    }
 }
