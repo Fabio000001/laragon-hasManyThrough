16b018884ea634b71652ff848e4961fbd11097a0
Fabio Acosta <Fabio@Acosta.com>
2025-01-09 20:44:51 +0000

    Factories y seeder terminados

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Country;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CountrySeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Country::factory(10)->create();
+        User::factory(10)->create();
     }
 }
