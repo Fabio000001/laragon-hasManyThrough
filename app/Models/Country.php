9d24b7c11d485750c244e38da992abece5430daa
Fabio Acosta <Fabio@Acosta.com>
2025-01-09 21:00:48 +0000

    Relaciones arregladas

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -12,11 +12,11 @@ class Country extends Model
 
     public function users()
     {
-        $this->hasMany(User::class);
+        return $this->hasMany(User::class);
     }
 
     public function posts()
     {
-        $this->hasManyThrough(Post::class, User::class);
+        return $this->hasManyThrough(Post::class, User::class);
     }
 }
